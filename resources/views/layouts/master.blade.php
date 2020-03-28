92e99a9ddabe862638f3ba06b0fdbee222bfaa72
mdashikulislam <ashik.nwu@gmail.com>
2020-03-28 20:33:49 +0600

    Custom Color for Font Awesome Icons and How Logout Works in Laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -66,7 +66,7 @@
 
                     <li class="nav-item">
                         <router-link to="/dashboard" class="nav-link">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <i class="nav-icon fas fa-tachometer-alt blue"></i>
                             <p>
                                 Dashboard
                             </p>
@@ -75,7 +75,7 @@
 
                     <li class="nav-item has-treeview">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-cog"></i>
+                            <i class="nav-icon fas fa-cog green"></i>
                             <p>
                                 Management
                                 <i class="right fa fa-angle-left"></i>
@@ -84,13 +84,13 @@
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
                                 <a href="#" class="nav-link active">
-                                    <i class="fa fa-circle-o nav-icon"></i>
+                                    <i class="fa fa-circle-o nav-icon purple"></i>
                                     <p>Active Page</p>
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
-                                    <i class="fa fa-circle-o nav-icon"></i>
+                                    <i class="fa fa-circle-o nav-icon purple"></i>
                                     <p>Inactive Page</p>
                                 </a>
                             </li>
@@ -98,19 +98,26 @@
                     </li>
                     <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
-                            <i class="nav-icon fas fa-user"></i>
+                            <i class="nav-icon fas fa-user purple"></i>
                             <p>
                                 Profile
                             </p>
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-power-off"></i>
+                        <a href="{{route('logout')}}" class="nav-link" onclick="
+                                event.preventDefault();
+                                document.getElementById('logout-form').submit();
+                            ">
+                            <i class="nav-icon fas fa-power-off teal"></i>
                             <p>
                                 Logout
                             </p>
                         </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </nav>
@@ -138,7 +145,7 @@
         <!-- Default to the left -->
         <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
     </footer>
-    
+
 </div>
 <!-- ./wrapper -->
 
