167bf288c63503cf28e35b4061a30d4aabd4065d
mdashikulislam <ashik.nwu@gmail.com>
2020-03-30 21:45:39 +0600

    Using VFrom for Back-end Error Handling

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,16 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
+import { Form, HasError, AlertError } from 'vform';
 
-Vue.use(VueRouter)
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+
+
+
+Vue.use(VueRouter);
 
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -59,13 +59,44 @@
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="addNewModalLabel">Modal title</h5>
+                        <h5 class="modal-title" id="addNewModalLabel">Add New User</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <div class="form-group">
+                            <input v-model="form.name" type="text" name="name"
+                                   placeholder="Enter Name"
+                                   class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                            <has-error :form="form" field="name"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <input v-model="form.email" type="email" name="email"
+                                   placeholder="Enter Email Address"
+                                   class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                            <has-error :form="form" field="email"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <textarea v-model="form.bio" name="bio" placeholder="Short bio for user (optional)" class="form-control" :class="{ 'is-invalid': form.errors.has('bio')}"></textarea>
+                            <has-error :form="form" field="bio"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <select name="type" v-model="form.type" class="form-control" :class="{ 'is-invalid': form.errors.has('type')}">
+                                <option value="">Select user type</option>
+                                <option value="admin">Admin</option>
+                                <option value="user">Standerd User</option>
+                                <option value="author">Author</option>
+                                <has-error :form="form" field="type"></has-error>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input v-model="form.password" type="password" name="password"
+                                   placeholder="Enter Password"
+                                   class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                            <has-error :form="form" field="password"></has-error>
+                        </div>
+
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
@@ -80,6 +111,19 @@
 
 <script>
     export default {
+
+        data(){
+          return {
+              form : new Form({
+                  name:'',
+                  email:'',
+                  password:'',
+                  type:'',
+                  bio:'',
+                  photo:''
+              })
+          }
+        },
         mounted() {
             console.log('Component mounted.')
         }
